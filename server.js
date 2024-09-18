89bb9cb24a9729269833e30d3b12766b417a011c
Dhanam-Bheeshma <dhanamveera@gmail.com>
2024-09-18 18:26:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
